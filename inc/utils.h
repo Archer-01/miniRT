91302e379507e03d1d731282c189077b06eccb25
Hamza Haddani <hhamza@student.1337.ma>
2022-11-01 17:10:20 +0100

    chore: Add utils

---
+++ inc/utils.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/01 16:57:45 by hhamza            #+#    #+#             */
+/*   Updated: 2022/11/01 17:09:00 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <math.h>
+# include <pthread.h>
+# include <stdbool.h>
+
+# define EPSILON 0.00001f
+
+bool	float_eq(float a, float b);
+
+#endif
