703ca91df793f03a869065822a4ddd44cbebb196
Hamza Haddani <hhamza@student.1337.ma>
2022-11-03 20:30:07 +0100

    feat: Add pattern
    Add color_at functions (Checkerboard & Gradient)

---
+++ inc/pattern.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pattern.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 19:13:25 by hhamza            #+#    #+#             */
+/*   Updated: 2022/11/03 20:32:48 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PATTERN_H
+# define PATTERN_H
+
+# include "color.h"
+# include "shape.h"
+# include "canvas.h"
+
+typedef struct s_uv
+{
+	float	u;
+	float	v;
+}				t_uv;
+
+typedef struct s_shape		t_shape;
+typedef struct s_pattern	t_pattern;
+typedef t_color				(*t_color_at_fun)(t_pattern, t_shape *, t_tuple);
+typedef t_uv				(*t_uv_at_fun)(t_tuple);
+
+typedef struct s_pattern
+{
+	t_color_at_fun	color_at;
+	t_uv_at_fun		uv_at;
+	size_t			width;
+	size_t			height;
+	t_color			primary;
+	t_color			secondary;
+	t_canvas		canvas;
+}				t_pattern;
+
+t_pattern	pattern(void);
+
+t_color		color_at_checker(t_pattern pattern, t_shape *shape, t_tuple point);
+t_color		color_at_texture(t_pattern pattern, t_shape *shape, t_tuple point);
+
+#endif
---
+++ src/color_at.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color_at.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 19:36:45 by hhamza            #+#    #+#             */
+/*   Updated: 2022/11/03 20:29:08 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pattern.h"
+
+t_color	color_at_checker(t_pattern pattern, t_shape *shape, t_tuple point)
+{
+	t_tuple	object_point;
+	t_uv	uv;
+	int		x;
+	int		y;
+
+	object_point = matrix_mult_tuple(shape_inverse_transform(shape), point);
+	uv = pattern.uv_at(object_point);
+	x = (int)(uv.u * pattern.width);
+	y = (int)(uv.v * pattern.height);
+	if ((x + y) % 2 == 0)
+	{
+		return (pattern.primary);
+	}
+	else
+	{
+		return (pattern.secondary);
+	}
+}
+
+t_color	color_at_texture(t_pattern pattern, t_shape *shape, t_tuple point)
+{
+	t_tuple		object_point;
+	t_uv		uv;
+	uint16_t	x;
+	uint16_t	y;
+
+	object_point = matrix_mult_tuple(shape_inverse_transform(shape), point);
+	uv = pattern.uv_at(object_point);
+	x = (uint16_t)(uv.u * (pattern.width - 1)) + 0.5f;
+	y = (uint16_t)(uv.v * (pattern.height - 1)) + 0.5f;
+	return (pattern.canvas.data[x][y]);
+}
