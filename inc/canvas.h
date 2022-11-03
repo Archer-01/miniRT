72b8e0003e942d35ef595ae7fd7243f43fe35bca
Hamza Haddani <hhamza@student.1337.ma>
2022-11-03 21:43:44 +0100

    chore: Add some TODOs

--- inc/canvas.h
+++ inc/canvas.h
@@ -6,7 +6,7 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 17:15:41 by hhamza            #+#    #+#             */
-/*   Updated: 2022/11/01 18:43:00 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/11/03 21:36:42 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,7 @@ t_canvas	canvas(uint16_t width, uint16_t height);
 void		canvas_destroy(t_canvas *canvas);
 
 void		canvas_export_ppm(t_canvas canvas, char const *filename);
+// TODO: Add import PPM
+// TODO: Add export MLX
 
 #endif
