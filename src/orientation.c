c25b980e7b390ca73e9219db7bc0c1ff806852ed
Omar Aizab <omaraizab98@gmail.com>
2022-11-07 16:07:21 +0100

    fix: orientation matrix

--- src/orientation.c
+++ src/orientation.c
@@ -6,7 +6,7 @@
 /*   By: oaizab <oaizab@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 19:55:24 by oaizab            #+#    #+#             */
-/*   Updated: 2022/11/06 20:06:36 by oaizab           ###   ########.fr       */
+/*   Updated: 2022/11/07 16:06:13 by oaizab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@ static t_matrix	matrix_from_vec(t_tuple x, t_tuple y, t_tuple z)
 	t_matrix	m;
 
 	m.data[0][0] = x.x;
-	m.data[0][1] = x.y;
-	m.data[0][2] = x.z;
-	m.data[0][3] = 0;
-	m.data[1][0] = y.x;
-	m.data[1][1] = y.y;
-	m.data[1][2] = y.z;
-	m.data[1][3] = 0;
-	m.data[2][0] = z.x;
-	m.data[2][1] = z.y;
-	m.data[2][2] = z.z;
-	m.data[2][3] = 0;
+	m.data[1][0] = x.y;
+	m.data[2][0] = x.z;
 	m.data[3][0] = 0;
+	m.data[0][1] = y.x;
+	m.data[1][1] = y.y;
+	m.data[2][1] = y.z;
 	m.data[3][1] = 0;
+	m.data[0][2] = z.x;
+	m.data[1][2] = z.y;
+	m.data[2][2] = z.z;
 	m.data[3][2] = 0;
+	m.data[0][3] = 0;
+	m.data[1][3] = 0;
+	m.data[2][3] = 0;
 	m.data[3][3] = 1;
 	return (m);
 }
@@ -41,9 +41,9 @@ t_matrix	orientation(t_tuple direction)
 	t_tuple		right;
 	t_tuple		forward;
 
-	if (up.x == 0 && up.z == 0)
+	if (direction.x == 0 && direction.z == 0)
 	{
-		if (up.y < 0)
+		if (direction.y < 0)
 		{
 			up = vector(0, -1, 0);
 			right = vector(-1, 0, 0);
@@ -58,8 +58,8 @@ t_matrix	orientation(t_tuple direction)
 	else
 	{
 		up = tuple_normalize(direction);
-		right = tuple_cross(up, vector(0, 1, 0));
-		forward = tuple_cross(right, up);
+		forward = tuple_normalize(tuple_cross(vector(0, 1, 0), up));
+		right = tuple_cross(up, forward);
 	}
 	return (matrix_from_vec(right, up, forward));
 }
