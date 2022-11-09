900933ca84cee0f3aa02b31b82d5a773b7825e84
Hamza Haddani <hhamza@student.1337.ma>
2022-11-09 09:59:21 +0100

    fix(cone): normal_at

--- src/normal_at.c
+++ src/normal_at.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   normal_at.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oaizab <oaizab@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 19:52:24 by oaizab            #+#    #+#             */
-/*   Updated: 2022/11/07 17:14:33 by oaizab           ###   ########.fr       */
+/*   Updated: 2022/11/09 09:42:18 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ t_tuple	normal_at_plane(t_shape *plane, t_tuple world_point)
 	trans = matrix_transpose(&inverse);
 	world_n = matrix_mult_tuple(&trans, vector(0, 1, 0));
 	world_n.w = 0;
-	return (world_n);
+	return (tuple_normalize(world_n));
 }
 
 t_tuple	normal_at_cylinder(t_shape *cylinder, t_tuple world_point)
@@ -74,29 +74,29 @@ t_tuple	normal_at_cylinder(t_shape *cylinder, t_tuple world_point)
 
 t_tuple	normal_at_cone(t_shape *cone, t_tuple world_point)
 {
-	t_tuple		object_point;
+	t_tuple		obj_point;
 	t_tuple		object_normal;
 	t_tuple		w_normal;
 	t_matrix	inverse;
 	float		dist;
 
 	inverse = shape_inverse_transform(cone);
-	object_point = matrix_mult_tuple(&inverse, world_point);
-	dist = object_point.x * object_point.x + object_point.z * object_point.z;
-	if (dist < object_point.y * object_point.y \
-		&& object_point.y >= cone->max - EPSILON)
+	obj_point = matrix_mult_tuple(&inverse, world_point);
+	dist = obj_point.x * obj_point.x + obj_point.z * obj_point.z;
+	if (dist < obj_point.y * obj_point.y && obj_point.y >= cone->max - EPSILON)
 		object_normal = vector(0, 1, 0);
-	else if (dist < object_point.y * object_point.y \
-		&& object_point.y <= cone->min - EPSILON)
+	else if (dist < obj_point.y * obj_point.y \
+		&& obj_point.y <= cone->min + EPSILON)
 		object_normal = vector(0, -1, 0);
 	else
 	{
-		if (object_point.y > 0)
-			object_normal = vector(object_point.x, -sqrt(dist), object_point.z);
+		if (obj_point.y > 0)
+			object_normal = vector(obj_point.x, -sqrt(dist), obj_point.z);
 		else
-			object_normal = vector(object_point.x, sqrt(dist), object_point.z);
+			object_normal = vector(obj_point.x, sqrt(dist), obj_point.z);
 	}
 	inverse = matrix_transpose(&inverse);
 	w_normal = matrix_mult_tuple(&inverse, object_normal);
+	w_normal.w = 0;
 	return (tuple_normalize(vector(w_normal.x, w_normal.y, w_normal.z)));
 }
